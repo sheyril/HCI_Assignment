d4ce8e8f9136d98cf9e64fdf69122be599a2ca67
sheyril <sheyril,agarwal321@gmail.com>
2021-04-12 20:29:52 +0530

    adding some IDE test files

---
+++ analyse_sess.py
@@ -0,0 +1,6 @@
+# number of clicks to add a product to cart
+# number of times lost (can be seen by hovering maybe, going back to the home, time between clicks)
+# times did the user had to click the back button, or start fresh, or cancel a course of action
+
+
+
